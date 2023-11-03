c5bb3cac84894e2f2a8ef08e19ed7cfb8765e53c
Daniel Samuel <dansam5000@gmail.com>
2023-11-03 03:10:59 +0100

    Write test-TDD for wait_for_db commands

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+"""
+Django commands to pause execution until database is available
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django commands to pause execution until database is available"""
+    def handle(self, *args, **options):
+        pass
