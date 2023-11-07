f04b065430565771d7c549f0567d3a6f7e4d1224
Daniel Samuel <dansam5000@gmail.com>
2023-11-07 02:29:54 +0100

    Write failing test for user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,18 @@
+"""
+Test the models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    """Test models"""
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@example.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email, password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
