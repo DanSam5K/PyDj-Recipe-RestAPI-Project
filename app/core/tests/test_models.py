34d82fbad67aa4eb64baa8b66565b81c75e42d64
Daniel Samuel <dansam5000@gmail.com>
2023-11-08 01:43:48 +0100

    Test creating super user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,3 +35,12 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'sample123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com', 'sample123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
