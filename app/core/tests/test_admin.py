0d779497624582ac128300f813cae3543e6101e6
Daniel Samuel <dansam5000@gmail.com>
2023-11-09 17:10:03 +0100

    add failing TDD for user page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -37,3 +37,11 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        # /admin/core/user/add
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
