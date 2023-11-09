082359bb8db2d17694ca30f220317ccdedca5a98
Daniel Samuel <dansam5000@gmail.com>
2023-11-09 15:21:29 +0100

    Implement TDD by listing user with failing test

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,31 @@
+"""
+Tests for the admin modification
+"""
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Test the admin site"""
+    def setUp(self):
+        """Set up the test create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='user@example.com',
+            password='testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='admin@example.com',
+            password='testpass123',
+            name='Test user full name'
+        )
+
+    def test_users_list(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
