05c0e1913c0e29fa6184eb426dde0e275bbe8b96
Daniel Samuel <dansam5000@gmail.com>
2023-11-09 16:43:50 +0100

    Implementation a passing test

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,31 @@
-# from django.contrib import admin
+"""
+Django Core admin module
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """This is the user admin class"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        ('Personal Info', {
+            'fields': ('name',)
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser')
+        }),
+        ('Important Dates', {
+            'fields': ('last_login',)
+        })
+    )
+    readonly_fields = ['last_login']
+
+
+admin.site.register(models.User, UserAdmin)
