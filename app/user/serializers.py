b22ee61a5bd8c28c3f1a0b2fcdcbcf81f5cbe995
Daniel Samuel <dansam5000@gmail.com>
2023-12-18 19:27:53 +0100

    write test for manage user API

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -24,6 +24,7 @@ class UserSerializer(serializers.ModelSerializer):
         return get_user_model().objects.create_user(**validated_data)
 
 
+
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object."""
     email = serializers.EmailField()
