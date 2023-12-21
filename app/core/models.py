a5a9502d6218bee27823073098a91b9d0c96cf97
Daniel Samuel <dansam5000@gmail.com>
2023-12-21 16:58:58 +0100

    implement recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 This file is used to create models for the core app.
 """
+from django.conf import settings
 
 from django.db import models
 from django.contrib.auth.models import (
@@ -54,3 +55,20 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # This is the username field
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        """Return the string representation of the recipe"""
+        return self.title
