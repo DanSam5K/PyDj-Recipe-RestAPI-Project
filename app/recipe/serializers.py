48d4fd3872142d8ab9bb685afefb1766686e9f46
Daniel Samuel <dansam5000@gmail.com>
2024-01-11 19:52:09 +0100

    implement Ingredients listings Api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -6,8 +6,16 @@ from rest_framework import serializers
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for ingredient objects"""
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name',)
+        read_only_fields = ('id',)
 
 class TagSerializer(serializers.ModelSerializer):
     """Serializer for tag objects"""
