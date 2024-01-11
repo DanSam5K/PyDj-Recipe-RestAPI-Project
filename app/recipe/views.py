5295fb2d49926e34b4196b591bf092fdaf20e351
Daniel Samuel <dansam5000@gmail.com>
2024-01-11 20:04:35 +0100

    write test for deleting ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -54,7 +54,7 @@ class TagViewSet(mixins.DestroyModelMixin,
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(mixins.UpdateModelMixin,
+class IngredientViewSet(mixi, mixins.UpdateModelMixin,
                         mixins.ListModelMixin,
                         viewsets.GenericViewSet):
     """Manage ingredients in the database"""
